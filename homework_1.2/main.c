10816d41adf25eb7c3a7a937f9d9cb24a159f9eb
LyazginD <71127710+SevenSixtyTwo@users.noreply.github.com>
2024-11-05 20:17:28 +0500

    Added homework_1.2 project

---
+++ homework_1.2/main.c
@@ -0,0 +1,72 @@
+#include "stm32f30x_gpio.h"
+#include "stm32f30x_rcc.h"
+#include "stm32f30x.h"
+
+// Exercise 2
+// 1 LED blinks with some frequency
+// Pressing the button will change blinking frequency
+
+void _delay(uint32_t f){
+	uint32_t i = 0;
+	f *= 7.2;
+
+	for(i = 0; i <= f; ++i){};
+}
+
+int main(void)
+{
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOE, ENABLE);
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+
+	GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
+
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	uint8_t f = 1;
+
+    while(1)
+    {
+    	if(f && !GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)){
+			GPIO_SetBits(GPIOE, GPIO_Pin_8);
+			_delay(50000);
+			GPIO_ResetBits(GPIOE, GPIO_Pin_8);
+			_delay(50000);
+    	}
+
+    	if(!f && !GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)){
+			GPIO_SetBits(GPIOE, GPIO_Pin_8);
+			_delay(80000);
+			GPIO_ResetBits(GPIOE, GPIO_Pin_8);
+			_delay(80000);
+		}
+
+    	if(!f && GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)){
+    		_delay(50000);
+    		if(!f && GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)){
+				f = 1;
+			}
+    		while(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)){};
+    	}
+
+    	if(f && GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)){
+			_delay(50000);
+			if(f && GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)){
+				f = 0;
+			}
+    		while(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)){};
+		}
+	}
+}
